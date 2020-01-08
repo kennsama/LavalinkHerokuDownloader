25a00bd3a231e037c062c38bd3804f5d11e53d8e
Luís Felipe do Nascimento <38709167+LuisFelip3@users.noreply.github.com>
2020-01-08 16:59:15 -0400

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -3,137 +3,139 @@ Lavalink on Heroku bootstrap script
 Credit to diniboy for sed script
 """
 
-from os import system, environ
+from os import system, environ, path, remove
+import requests
 
 
 class LavalinkBootstrap:
+	"""
+	Class we're using to get Lavalink working on Heroku
+	"""
 
-    """
-    Class we're using to get Lavalink working on Heroku
-    """
+	def __init__(self):
 
-    def __init__(self):
+		"""
+		Doing important stuff here
+		"""
 
-        """
-        Doing important stuff here
-        """
-        self._lavalink_version = "3.2.2" if not environ.get('LAVALINK_VERSION') else environ.get('LAVALINK_VERSION')
+		self.download_command = "wget https://github.com/Frederikam/Lavalink/releases/download/3.2.2/Lavalink.jar"
 
-        self.download_command = f"wget https://github.com/Frederikam/Lavalink/releases/download/{self._lavalink_version}/Lavalink.jar"
+		self.replace_port_command = 'sed -i "s|DYNAMICPORT|$PORT|" application.yml'
 
-        self.replace_port_command = 'sed -i "s|DYNAMICPORT|$PORT|" application.yml'
+		self.replace_password_command = 'sed -i "s|DYNAMICPASSWORD|$PASSWORD|" application.yml'
+		self.replace_password_command_no_password = 'sed -i "s|DYNAMICPASSWORD|youshallnotpass|" application.yml'
 
-        self.replace_password_command = 'sed -i "s|DYNAMICPASSWORD|$PASSWORD|" application.yml'
-        self.replace_password_command_no_password = 'sed -i "s|DYNAMICPASSWORD|youshallnotpass|" application.yml'
-        
-        self._additional_options = environ.get(
-            "ADDITIONAL_JAVA_OPTIONS"
-        ) # Heroku provides basic Java configuration based on dyno size, no need in limiting memory
-    
-        self.run_command = f"java -jar Lavalink.jar {self._additional_options}" # User-provided config, will override heroku's
+		self._additional_options = environ.get(
+			"ADDITIONAL_JAVA_OPTIONS"
+		)  # Heroku provides basic Java configuration based on dyno size, no need in limiting memory
 
-    def replace_password_and_port(self):
+		self.run_command = "java -jar Lavalink.jar {}".format(self._additional_options)  # User-provided config, will override heroku's
 
-        """
-        Replacing password and port in application.yml
-        """
+	def replace_password_and_port(self):
 
-        print(
-            "[INFO] Replacing port..."
-        )
+		"""
+		Replacing password and port in application.yml
+		"""
 
-        try:
-            
-            system(
-                self.replace_port_command
-            )
+		print(
+			"[INFO] Replacing port..."
+		)
 
-            if not environ.get("PASSWORD"):
+		try:
 
-                print(
-                    """
-                    [WARNING] You have not specified your Lavalink password in config vars. To do this, go to settings 
-                    and set the PASSWORD environment variable
-                    """
-                )
-    
-                return system(
-                    self.replace_password_command_no_password
-                )
-            
-            system(
-                self.replace_password_command
-            )
+			system(
+				self.replace_port_command
+			)
 
-        except BaseException as exc:
+			if not environ.get("PASSWORD"):
+				print(
+					"""
+					[WARNING] You have not specified your Lavalink password in config vars. To do this, go to settings
+					and set the PASSWORD environment variable
+					"""
+				)
 
-            print(
-                f"[ERROR] Failed to replace port/password. Info: {exc}"
-            )
+				return system(
+					self.replace_password_command_no_password
+				)
 
-        else:
+			system(
+				self.replace_password_command
+			)
 
-            print(
-                "[INFO] Done. Config is ready now"
-            )
+		except BaseException as exc:
 
-    def download(self):
+			print(
+				"[ERROR] Failed to replace port/password. Info: {}.format(exc)"
+			)
 
-        """
-        Downloads latest release of Lavalink
-        """
+		else:
 
-        print(
-            "[INFO] Downloading latest release of Lavalink..."
-        )
-        
-        try:
-            
-            system(
-                self.download_command
-            )
-        
-        except BaseException as exc:
+			print(
+				"[INFO] Done. Config is ready now"
+			)
 
-            print(
-                f"[ERROR] Lavalink download failed. Info: {exc}"
-            )
+	def download(self):
 
-        else:
-        
-            print(
-                "[INFO] Lavalink download OK"
-            )
-    
-    def run(self):
+		"""
+		Downloads latest release of Lavalink
+		"""
 
-        """
-        Runs Lavalink instance
-        """
+		print(
+			"[INFO] Downloading latest release of Lavalink..."
+		)
 
-        self.download()
-        self.replace_password_and_port()
+		try:
+			if not path.isfile("application.yml"):
+				print("[INFO] Downloading application.yml...")
+				system("wget -q https://raw.githubusercontent.com/F4stZ4p/HLavalink/master/application.yml")
+			system(
+				self.download_command
+			)
 
-        print(
-            "[INFO] Starting Lavalink..."
-        )
+		except BaseException as exc:
 
-        try:
+			print(
+				"[ERROR] Lavalink download failed. Info: {}".format(exc)
+			)
 
-            system(
-                self.run_command
-            )
-        
-        except BaseException as exc:
+		else:
 
-            print(
-                f"[ERROR] Failed to start Lavalink. Info: {exc}"
-            )
+			print(
+				"[INFO] Lavalink download OK"
+			)
+
+	def run(self):
+
+		"""
+		Runs Lavalink instance
+		"""
+
+		self.download()
+		self.replace_password_and_port()
+
+		print(
+			"[INFO] Starting Lavalink..."
+		)
+
+		try:
+
+			system(
+				self.run_command
+			)
+
+		except BaseException as exc:
+
+			print(
+				"[ERROR] Failed to start Lavalink. Info: {}".format(exc)
+			)
+
 
 if __name__ == "__main__":
 
-    """
-    Starts our instance
-    """
+	"""
+	Starts our instance
+	"""
 
-    LavalinkBootstrap().run()
+	LavalinkBootstrap().run()
+
